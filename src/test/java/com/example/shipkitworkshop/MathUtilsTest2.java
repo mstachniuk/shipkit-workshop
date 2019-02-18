5255bb584819cead6fa9c3dc4b3c58942fbc9e85
pptiucha <piotr.pitucha@gmail.com>
2019-02-18 13:36:29 +0100

    Added unit tests 2

--- src/test/java/com/example/shipkitworkshop/MathUtilsTest2.java
+++ src/test/java/com/example/shipkitworkshop/MathUtilsTest2.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 
 public class MathUtilsTest2 {
-ś
+
     @Test
     public void shouldNotCreateTriangle2() {
         assertFalse(MathUtils.isTriangle(5, 5, 10));
