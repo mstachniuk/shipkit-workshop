bf77e0905647a475e5767c60c95f58bd012f09a4
pptiucha <piotr.pitucha@gmail.com>
2019-02-18 11:44:51 +0100

    Floating point support #3

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -2,14 +2,13 @@ package com.example.shipkitworkshop;
 
 public class MathUtils {
 
-    /**
-     *
-     * @param a
-     * @param b
-     * @param c
-     * @return
-     */
     public static boolean isTriangle(int a, int b, int c) {
+        return a + b > c
+                && a + c > b
+                && b + c > a;
+    }
+
+    public static boolean isTriangle(float a, float b, float c) {
         return a + b > c
                 && a + c > b
                 && b + c > a;
