ede6d05bb64594697576cc919c7752da6f0898db
pptiucha <piotr.pitucha@gmail.com>
2019-02-18 11:34:02 +0100

    change 2

--- src/main/java/com/example/shipkitworkshop/MathUtils.java
+++ src/main/java/com/example/shipkitworkshop/MathUtils.java
@@ -2,6 +2,13 @@ package com.example.shipkitworkshop;
 
 public class MathUtils {
 
+    /**
+     *
+     * @param a
+     * @param b
+     * @param c
+     * @return
+     */
     public static boolean isTriangle(int a, int b, int c) {
         return a + b > c
                 && a + c > b
